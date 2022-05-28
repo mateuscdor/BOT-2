2a775f1045757066f61c8da03e3a4e05e887da42
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-05-27 19:44:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('OWNER\nVIP\n✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹', {
+say('OWNER\nVIP\nGHOST', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
