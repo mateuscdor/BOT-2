d079f75d17cc4540deef00c619576b48cfcdc5d0
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-05-28 11:43:28 -0500

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -5,7 +5,7 @@ this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 let pp = './media/menus/Menu2.jpg'
 let str = `🧡 *Eso son los contactos para ti.*\n💜 *That's the contacts for you.*`
 
-conn.sendHydrated(m.chat, str, wm, pp, 'https://www.instagram.com/gata_dios', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
+conn.sendHydrated(m.chat, str, wm, pp, 'http://wa.me/+51996089079', 'ᵛⁱᵖ', null, null, [
 ['𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
 ['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', '/menu']
 ], m,)
