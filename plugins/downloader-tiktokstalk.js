f0291d6ddb9bd5b10e85af0cf75e26c0c892ce00
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-05-28 19:30:16 -0500

    Update downloader-tiktokstalk.js

--- plugins/downloader-tiktokstalk.js
+++ plugins/downloader-tiktokstalk.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
-let handler = async(m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚃𝙸𝙺𝚃𝙾𝙺*', m)
+let handler = async(m, { conn, text, command, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 𝘿𝙀 𝙏𝙄𝙆𝙏𝙊𝙆 𝙎𝙄𝙉 𝙐𝙎𝘼𝙍 "@"\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} mundo_dos_animes81*\n\n𝙏𝙔𝙋𝙀 𝙔𝙊𝙐𝙍 𝙏𝙄𝙆𝙏𝙊𝙆 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀 𝙒𝙄𝙏𝙃𝙊𝙐𝙏 𝙐𝙎𝙄𝙉𝙂 "@"\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} universo_editx*`, m)
 try {
 let res = await fetch(`https://api.lolhuman.xyz/api/stalktiktok/${text}?apikey=9b817532fadff8fc7cb86862`)
 let res2 = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=9b817532fadff8fc7cb86862`
@@ -8,19 +8,42 @@ let json = await res.json()
 if (res.status !== 200) throw await res.text()
 if (!json.status) throw json
 let thumb = await (await fetch(json.result.user_picture)).buffer()
-let Mystic = `
-*𝚄𝚂𝚄𝙰𝚁𝙸𝙾:* ${json.result.username}
-*𝙽𝙾𝙼𝙱𝚁𝙴:* ${json.result.nickname}
-*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚁𝙴𝚂:* ${json.result.followers}
-*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚂:* ${json.result.followings}
-*𝙻𝙸𝙺𝙴𝚂:* ${json.result.likes}
-*𝚅𝙸𝙳𝙴𝙾𝚂:* ${json.result.video}
-*𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:* ${json.result.bio}
+let gata = `
+👤 𝙐𝙎𝙐𝘼𝙍𝙄𝙊(𝘼) | 𝙐𝙎𝙀𝙍
+${json.result.username}
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✨ 𝙉𝙊𝙈𝘽𝙍𝙀 | 𝙉𝘼𝙈𝙀
+${json.result.nickname}
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+✅ 𝙎𝙀𝙂𝙐𝙄𝘿𝙊𝙍𝙀𝙎 | 𝙁𝙊𝙇𝙇𝙊𝙒𝙀𝙍𝙎 
+${json.result.followers}
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+❇️ 𝙎𝙀𝙂𝙐𝙄𝘿𝙊𝙎 | 𝙁𝙊𝙇𝙇𝙊𝙒𝙄𝙉𝙂
+${json.result.followings}
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+❤️ 𝙈𝙀 𝙂𝙐𝙎𝙏𝘼 | 👍 𝙇𝙄𝙆𝙀𝙎
+${json.result.likes}
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+🎁 𝙋𝙐𝘽𝙇𝙄𝘾𝘼𝘾𝙄𝙊𝙉𝙀𝙎 | 𝙋𝙊𝙎𝙏𝙎
+${json.result.video}
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+👀 𝘽𝙄𝙊𝙂𝙍𝘼𝙁Í𝘼 | 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝙏𝙄𝙊𝙉
+${json.result.bio}
 `.trim()
-conn.sendFile(m.chat, res2, 'error.jpg', Mystic, m, false)
+await conn.sendFile(m.chat, res2, 'error.jpg', gata, m, false)
 } catch (e) {
-throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝚂𝙴 𝙴𝙲𝙾𝙽𝚃𝚁𝙾 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝙶𝚁𝙴𝚂𝙰𝙳𝙾*'
-}}
+throw `${fg}𝙉𝙊 𝙎𝙀 𝙀𝙉𝘾𝙊𝙉𝙏𝙍𝙊 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙎𝙐𝘼𝙍𝙄𝙊.\n\n𝙉𝙊 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀 𝙁𝙊𝙐𝙉𝘿.`
+}
+let info = `🔰 *Infórmate sobre las Novedades y recuerda tener la última versión.*\n\n🔰 *Find out about what's new and remember to have the latest version.*
+  `.trim()
+  
+await conn.sendHydrated(m.chat, info, wm, null, ig, 'ᵛⁱᵖ', null, null, [
+['𝙈𝙚𝙣𝙪 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨 🌀', '#descargasmenu'],
+['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,)  
+
+}
 handler.help = ['tiktokstalk'].map(v => v + ' <username>')
 handler.tags = ['stalk']
 handler.command = /^(tiktokstalk|ttstalk)$/i
