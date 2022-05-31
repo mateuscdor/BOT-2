c913b45c559322af0b799f4ee6c6f860abd05ce3
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-05-31 00:36:47 -0500

    Update propietario(a)-banuser.js

--- plugins/propietario(a)-banuser.js
+++ plugins/propietario(a)-banuser.js
@@ -6,7 +6,7 @@ else who = m.chat
 if (!who) throw `${mg}𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙀 𝘼 𝘼𝙇𝙂𝙐𝙄𝙀𝙉 𝙋𝘼𝙍𝘼 𝘽𝘼𝙉𝙀𝘼𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag*/n/n𝙏𝘼𝙂 𝙎𝙊𝙈𝙀𝙊𝙉𝙀 𝙏𝙊 𝘽𝘼𝙉\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} @tag*`
 let users = global.db.data.users
 users[who].banned = true
-conn.reply(m.chat, `${eg}❗ 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗕𝗔𝗡𝗘𝗔𝗗𝗢 ❗\n ➖Registrado en blocklist \n ➖No accede a comandos \n ➖BOT ignora a `, m)
+conn.reply(m.chat, `${eg} 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗕𝗔𝗡𝗘𝗔𝗗𝗢\n Registrado en blocklist \n No accede a comandos \n BOT ignora a `, m)
 }
 handler.help = ['banuser']
 handler.tags = ['owner']
