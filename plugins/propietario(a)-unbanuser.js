4be8bb327202b883c8c8fe8cfa4d56fa05a0dccb
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-06-01 16:30:44 -0500

    Update propietario(a)-unbanuser.js

--- plugins/propietario(a)-unbanuser.js
+++ plugins/propietario(a)-unbanuser.js
@@ -10,7 +10,7 @@ users[who].banned = false
 let mentionedJid = [who]
 let username = conn.getName(who)
 
-conn.reply(m.chat, `${eg}❗ 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗗𝗘𝗦𝗕𝗔𝗡𝗘𝗔𝗗𝗢 ❗\n\n ✓ Acceso al BOT .\n✓ Grupos y chat pv.\n✓ Disfrute del BOT:\n      ${username}. \n\n*𝙰𝚃𝚃𝙴: 𝗕𝗢𝗧 ${gt}*`, m)
+conn.reply(m.chat, `${eg}❗ 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗗𝗘𝗦𝗕𝗔𝗡𝗘𝗔𝗗𝗢 ❗\n\n✓ Acceso al BOT .\n✓ Grupos y chat pv.\n✓ Disfrute del BOT:\n      ${username}. \n\n*𝙰𝚃𝚃𝙴: 𝗕𝗢𝗧 ${gt}*`, m)
 }
 handler.help = ['unban']
 handler.tags = ['owner']
