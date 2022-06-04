69f27a50ad7dc28f23721735f690d04c647efda6
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-04 18:59:54 -0500

    Create Prueba.js

---
+++ plugins/Prueba.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn, command, text, usedPrefix }) => { 
+if (!text) throw `Lo uso mal, vuelva a intentar.` 
+
+if (command == 'juego1') {
+ m.reply(`Resultado 1`) 
+}
+  
+  if (command == 'juego2') {
+    m.reply(`Resultado 2`) 
+    
+  }   
+    
+    if (command == 'juego3') {
+       m.reply(`Resultado 3`) 
+      
+    }
+  
+handler.help = ['juego1', 'juego1', 'juego1'].map(v => v + ' @ | nombre')
+handler.tags = ['ejemplo']
+handler.command = ['juego1', 'juego2', 'juego3']
+handler.exp = false
+export default handler
+  
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
