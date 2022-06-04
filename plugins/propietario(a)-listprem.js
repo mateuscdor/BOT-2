a7f833ce5c78dbbf6d1f7d54e344ba2ece97446b
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-06-04 15:39:11 -0500

    Update propietario(a)-listprem.js

--- plugins/propietario(a)-listprem.js
+++ plugins/propietario(a)-listprem.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 let handler = async (m, { conn }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
 
-await conn.reply(m.chat, `*╭━[ 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙑𝙄𝙋 | 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 𝙐𝙎𝙀𝙍𝙎 ]━⬣*\n*┃*\n` + prem.map(v => '*┃* ➥ 💎 @' + v.replace(/@.+/, ' 💎')).join`\n`, m, { contextInfo: { mentionedJid: prem } }) 
+await conn.reply(m.chat, `*╭━[ 𝗟𝗜𝗦𝗧𝗔 | 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙑𝙄𝙋 ]━⬣*\n*┃*\n` + prem.map(v => '*┃* ➥ 💎 @' + v.replace(/@.+/, ' 💎')).join`\n`, m, { contextInfo: { mentionedJid: prem } }) 
    
   let text = `𝙌𝙐𝙀 𝙋𝙍𝙊!! 😼 𝙏𝙃𝙀𝙔 𝘼𝙍𝙀 𝙂𝙍𝙀𝘼𝙏!! ✨`.trim()   
 await conn.reply(m.chat, text, m, {
