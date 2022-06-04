93b17adc9c5fb263decda74ddaf1503f8d1231d2
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-06-04 15:44:25 -0500

    Update propietario(a)-listprem.js

--- plugins/propietario(a)-listprem.js
+++ plugins/propietario(a)-listprem.js
@@ -4,7 +4,7 @@ let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').f
 
 await conn.reply(m.chat, `*╭━[ 𝗟𝗜𝗦𝗧𝗔 | 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙑𝙄𝙋 ]━⬣*\n*┃*\n` + prem.map(v => '*┃* ➥ 💎 @' + v.replace(/@.+/, ' 💎')).join`\n`, m, { contextInfo: { mentionedJid: prem } }) 
    
-await conn.sendHydrated(m.chat, null, wm, null, null, null, null, null, [
+await conn.sendHydrated(m.chat, null, null, null, null, null, null, [
 ['𝙏𝙤𝙥 𝙐𝙨𝙪𝙖𝙧𝙞𝙤𝙨 | 𝙐𝙨𝙚𝙧𝙨 🏆', '.top'],
 ['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '/cuentasgb'],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
