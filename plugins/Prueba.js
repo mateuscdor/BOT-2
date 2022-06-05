20d7b3bc6809f397b38a5bfbe0fc507d8e1163ea
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-05 01:13:52 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,13 +1,15 @@
 let handler = async (m, { command, text }) => { 
 if (!text) throw `Lo uso mal, vuelva a intentar.` 
 let pp = './Menu2.jpg'
+if (command == 'go') {
 //let id = text.join(' ')
+let ejemplo = `Respuesta 1`
 
-conn.sendHydrated(m.chat, `Respuesta 1`, null, pp, null, null, null, null, [
+conn.sendHydrated(m.chat, ejemplo, null, pp, null, null, null, null, [
 ['Juego 1', `.juego1 whats`],
 ['Juego 2', `.juego2 whatsat`],
 ['Juego 3', `juego3 whatsapp`]
-], m,)
+], m,) }
 
 if (command == 'juego1') {
  if (text == 'whats') {
@@ -29,7 +31,7 @@ if (command == 'juego1') {
 //handler.tags = ['Prueba']
 
 //export default handler  
-handler.help = ['juego1', 'juego2', 'juego3']
+handler.help = ['go', 'juego1', 'juego2', 'juego3']
 handler.tags = ['ejemplo']
 handler.command = ['go', 'juego1', 'juego2', 'juego3'] //'whats', 'whatsat', 'whatsapp'
 
