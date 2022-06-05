4a6348ae342e89c897e9d707aedb49c97a541ab4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-04 19:29:02 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,16 +1,16 @@
 let handler = async (m, { command, text }) => { 
 if (!text) throw `Lo uso mal, vuelva a intentar.` 
 
-if (command == 'juego1') {
+if (command == 'juego1 whats') {
  m.reply(`Resultado 1`) 
 }
   
-  if (command == 'juego2') {
+  if (command == 'juego2 whatsat') {
     m.reply(`Resultado 2`) 
     
   }   
     
-    if (command == 'juego3') {
+    if (command == 'juego3 whatsapp') {
        m.reply(`Resultado 3`) 
       
     }}
