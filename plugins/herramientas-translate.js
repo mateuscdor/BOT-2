016e84f90cbb9dc01ab4530f1380a12066360ff0
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-06-06 18:17:57 -0500

    Update herramientas-translate.js

--- plugins/herramientas-translate.js
+++ plugins/herramientas-translate.js
@@ -21,7 +21,7 @@ result = await translate(`${text}`, {
 tld,
 to: defaultLang, })
 } finally {
-m.reply('*Traducción:* ' +  result[0]) }}
+m.reply('*🔰 Traducción:* ' +  result[0]) }}
 handler.help = ['translate', 'trad']
 handler.tags = ['General']
 handler.command = /^(tr(anslate)|trad?)$/i
