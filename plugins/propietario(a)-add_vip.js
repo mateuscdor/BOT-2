bb97b90648e0d1a33cc4dc7618c320df3d579d81
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-06-06 20:13:42 -0500

    Update propietario(a)-add_vip.js

--- plugins/propietario(a)-add_vip.js
+++ plugins/propietario(a)-add_vip.js
@@ -5,13 +5,13 @@ else who = m.chat
 if (!who) throw `${mg}𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙀 𝘼 𝙇𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼 𝙌𝙐𝙀 𝙑𝘼 𝙎𝙀𝙍 𝙐𝙎𝙐𝘼𝙍𝙄𝙊(𝘼) 𝙑𝙄𝙋 💎\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag*\n\n𝙏𝘼𝙂 𝙏𝙃𝙀 𝙋𝙀𝙍𝙎𝙊𝙉 𝙒𝙃𝙊 𝙒𝙄𝙇𝙇 𝘽𝙀 𝘼 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 𝙐𝙎𝙀𝙍 💎\n\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} @tag*`
 if (global.prems.includes(who.split`@`[0])) throw `${iig}𝙀𝙇/𝙇𝘼 𝙐𝙎𝙐𝘼𝙍𝙄𝙊(𝘼) 𝙔𝘼 𝙀𝙎 𝙑𝙄𝙋 ✨\n\n𝙏𝙃𝙀 𝙐𝙎𝙀𝙍 𝙄𝙎 𝘼𝙇𝙍𝙀𝘼𝘿𝙔 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✨`
 global.prems.push(`${who.split`@`[0]}`)
-conn.reply(m.chat, `${eg} *✅@${who.split`@`[0]} 𝘼𝙃𝙊𝙍𝘼 𝙀𝙎 𝙐𝙎𝙐𝘼𝙍𝙄𝙊(𝘼) 𝙑𝙄𝙋.* \n\n *➖𝙉𝙊 𝙑𝘼 𝙏𝙀𝙉𝙀𝙍 𝙇𝙄𝙈𝙄𝙏𝙀𝙎 𝘾𝙊𝙉 𝗘𝗟 𝗕𝗢𝗧 ${gt}.*`, m, {
+conn.reply(m.chat, `${eg} *✅ @${who.split`@`[0]} 𝘼𝙃𝙊𝙍𝘼 𝙀𝙎 𝙐𝙎𝙐𝘼𝙍𝙄𝙊(𝘼) 𝙑𝙄𝙋.* \n\n *➖𝙉𝙊 𝙑𝘼 𝙏𝙀𝙉𝙀𝙍 𝙇𝙄𝙈𝙄𝙏𝙀𝙎 𝘾𝙊𝙉 𝗘𝗟 𝗕𝗢𝗧 ${gt}.*`, m, {
 contextInfo: {
 mentionedJid: [who]
 }})}
-handler.help = ['VIP <@user>']
+handler.help = ['addprem <@user>']
 handler.tags = ['owner']
-handler.command = /^(V|\+)IP$/i
+handler.command = /^(add|\+)prem$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
