6f739512d8cef2dde9f9fc34ab5bd875d4b7c6b7
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-06-08 12:55:25 -0500

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -1,16 +1,16 @@
 let handler = m => m
 
 handler.before = async function (m) {
-   if (m.sender.startsWith('212' || '212')) {
-   	global.db.data.users[m.sender].banned = true
+   if (m.sender.startsWith('1' || '1')) {
+   	global.db.data.users[m.sender].banned = false
 conn.reply('see u next time')
-conn.groupParticipantsUpdate(m.chat, [user], "remove")
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    }
    
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
 conn.reply('see u next time')
-conn.groupParticipantsUpdate(m.chat, [user], "remove")
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
     }
 
