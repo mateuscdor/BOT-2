372ac29726a1add2b08a7eee03902c133007e2ce
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-06-09 13:54:05 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -13,7 +13,7 @@ conn.sendHydrated(m.chat, ejemplo, null, pp, null, null, null, null, [
 
 if (command == 'nido') {
  if (text == 'caterpie') {
- m.reply(`Resultado 1`) (`hola`) }
+ m.reply(`Resultado 1`)(`hola`) }
 }
   
   if (command == 'juego2') {
