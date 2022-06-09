f2dc1056ab6eabea517e6678cd2ed6fb4eb9f02e
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-06-09 15:30:10 -0500

    Create 2comandos.js

---
+++ plugins/2comandos.js
@@ -0,0 +1,48 @@
+let handler = async (m, { command, text }) => { 
+//if (!text) throw `Lo uso mal, vuelva a intentar.` 
+let pp = './Menu2.jpg'
+if (command == 'abc') {
+//let id = text.join(' ')
+let ejemplo = `Respuesta 1`
+
+conn.sendHydrated(m.chat, ejemplo, null, pp, null, null, null, null, [
+['Caterpie', `.nido1 caterpie1`],
+['Ekans', `.juego22 whatsat1`],
+['Pikachu', `.juego3 whatsapp33`]
+], m,) }
+
+if (command == 'nido1') {
+ if (text == 'caterpie1') {
+ await m.reply(`Resultado 1`)
+ await m.reply(`Resultado 2`)
+ await m.reply(`hola0`) }
+}
+  
+  if (command == 'juego22') {
+ if (text == 'whatsat1') {
+ m.reply(`Resultado 2`) }
+}  
+    
+    if (command == 'juego33') {
+ if (text == 'whatsapp33') {
+ m.reply(`Resultado 3`) }
+}
+}
+ 
+//handler.help = handler.command = ['NIDO', 'juego2', 'juego3']
+//handler.tags = ['Prueba']
+
+//export default handler  
+handler.help = ['abc', 'NIDO1', 'juego22', 'juego33']
+handler.tags = ['ejemplo']
+handler.command = ['abc', 'nido1', 'juego22', 'juego33'] //'ABRA', 'whatsat', 'whatsapp'
+
+export default  handler
+  
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
