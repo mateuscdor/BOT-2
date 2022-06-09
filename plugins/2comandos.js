d12ce6adbaf170c9305d2d0a85999323cc0320e2
WHATSAPP-VIP <106401400+WHATSAPP-VIP@users.noreply.github.com>
2022-06-09 15:41:26 -0500

    Update 2comandos.js

--- plugins/2comandos.js
+++ plugins/2comandos.js
@@ -11,11 +11,16 @@ conn.sendHydrated(m.chat, ejemplo, null, pp, null, null, null, null, [
 ['Pikachu', `.juego3 whatsapp33`]
 ], m,) }
 
-if (command == 'nido1') {
- if (text == 'caterpie1') {
- await m.reply(`Resultado 1`)
+if (command == 'nido') {
+ if (text == 'caterpie') {
+ await conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝐍𝐈𝐃𝐎𝐒.',
+body: '✦҈͜͡➳👻𝕲𝔥𝔬𝔰𝔱•✓☆ۣۜۜ͜͡🌹',         
+previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
+sourceUrl: `http://wa.me/+51996089079`}}})
  await m.reply(`Resultado 2`)
- await m.reply(`hola0`) }
+ await m.reply(`hola`) }
 }
   
   if (command == 'juego22') {
